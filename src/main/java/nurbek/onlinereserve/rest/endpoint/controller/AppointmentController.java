b30d8d178835d2a20210a9309e1d2657cf7225ec
Nurbek Abduraximov <abduraximovnurbek0002@gmail.com>
2024-01-09 10:35:02 +0500

    second commit and changes

--- src/main/java/nurbek/onlinereserve/rest/endpoint/controller/AppointmentController.java
+++ src/main/java/nurbek/onlinereserve/rest/endpoint/controller/AppointmentController.java
@@ -3,8 +3,6 @@ package nurbek.onlinereserve.rest.endpoint.controller;
 // Abduraximov Nurbek  1/8/2024   5:41 PM
 
 import lombok.RequiredArgsConstructor;
-import nurbek.onlinereserve.base.BaseURI;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
